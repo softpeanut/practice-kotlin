94a11c7db9ab114badebeeb89239d2a2ca624841
KimBeomJin <sdpthf@naver.com>
2022-04-17 12:09:17 +0900

    🌱 :: User Facade

---
+++ src/main/kotlin/com/example/kopring/domain/user/facade/UserFacade.kt
@@ -0,0 +1,17 @@
+package com.example.kopring.domain.user.facade
+
+import com.example.kopring.domain.user.entity.User
+import com.example.kopring.domain.user.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class UserFacade(
+        private val userRepository: UserRepository
+) {
+
+    fun getById(id: Long): User {
+        return userRepository.findByIdOrNull(id) ?: throw RuntimeException("user not found")
+    }
+
+}
