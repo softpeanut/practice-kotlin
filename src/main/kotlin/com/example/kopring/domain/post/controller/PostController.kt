ecab7e206736b0ca12090d11e435f59b0e207429
KimBeomJin <sdpthf@naver.com>
2022-04-17 12:12:35 +0900

    🌧 :: post getById

--- src/main/kotlin/com/example/kopring/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/example/kopring/domain/post/controller/PostController.kt
@@ -2,12 +2,10 @@ package com.example.kopring.domain.post.controller
 
 import com.example.kopring.domain.post.entity.Post
 import com.example.kopring.domain.post.payload.request.SavePostRequest
+import com.example.kopring.domain.post.payload.response.PostResponse
 import com.example.kopring.domain.post.service.PostService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/post")
 @RestController
@@ -19,6 +17,11 @@ class PostController(
     @ResponseStatus(HttpStatus.CREATED)
     fun savePost(request: SavePostRequest): Post {
         return postService.save(request)
+    }
+
+    @GetMapping("/{post-id}")
+    fun getPostInfo(@PathVariable("post-id") postId: Long): PostResponse {
+        return postService.getById(postId)
     }
 
 }
--- src/main/kotlin/com/example/kopring/domain/post/service/PostService.kt
+++ src/main/kotlin/com/example/kopring/domain/post/service/PostService.kt
@@ -1,7 +1,9 @@
 package com.example.kopring.domain.post.service
 
 import com.example.kopring.domain.post.entity.Post
+import com.example.kopring.domain.post.facade.PostFacade
 import com.example.kopring.domain.post.payload.request.SavePostRequest
+import com.example.kopring.domain.post.payload.response.PostResponse
 import com.example.kopring.domain.post.repository.PostRepository
 import com.example.kopring.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
@@ -9,11 +11,12 @@ import org.springframework.stereotype.Service
 @Service
 class PostService(
         private val postRepository: PostRepository,
-        private val userFacade: UserFacade
+        private val userFacade: UserFacade,
+        private val postFacade: PostFacade
 ) {
 
     fun save(request: SavePostRequest): Post {
-        val user = userFacade.findById(1)
+        val user = userFacade.getById(1)
 
         val post = Post(
                 title = request.title,
@@ -22,6 +25,18 @@ class PostService(
         )
 
         return postRepository.save(post)
+    }
+
+    fun getById(id: Long): PostResponse {
+        val post: Post = postFacade.getById(id)
+
+        return PostResponse(
+                id = post.id,
+                title = post.title,
+                content = post.content,
+                writerName = post.user.name,
+                writerAge = post.user.age
+        )
     }
 
 }
