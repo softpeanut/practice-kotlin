b60853fa843f04ab5ce874325d258b043c9d7a20
KimBeomJin <sdpthf@naver.com>
2022-04-17 12:11:15 +0900

    🌧 :: savePost Controller mapping

--- src/main/kotlin/com/example/kopring/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/example/kopring/domain/post/controller/PostController.kt
@@ -3,7 +3,10 @@ package com.example.kopring.domain.post.controller
 import com.example.kopring.domain.post.entity.Post
 import com.example.kopring.domain.post.payload.request.SavePostRequest
 import com.example.kopring.domain.post.service.PostService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/post")
@@ -12,6 +15,8 @@ class PostController(
         private val postService: PostService
 ) {
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun savePost(request: SavePostRequest): Post {
         return postService.save(request)
     }
