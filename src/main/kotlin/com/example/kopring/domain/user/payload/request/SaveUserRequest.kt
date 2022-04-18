3836847fcc4bcf14cfd5738e7da577b6f80f5967
KimBeomJin <sdpthf@naver.com>
2022-04-18 23:07:52 +0900

    🌧 :: add column accountId & password

--- src/main/kotlin/com/example/kopring/domain/user/payload/request/SaveUserRequest.kt
+++ src/main/kotlin/com/example/kopring/domain/user/payload/request/SaveUserRequest.kt
@@ -1,6 +1,8 @@
 package com.example.kopring.domain.user.payload.request
 
 data class SaveUserRequest(
+        val accountId: String,
+        val password: String,
         val name: String,
         val email: String,
         val age: Int
