ccc3e43b8d50922c555b1c3d42b9ab2c33a9c147
KimBeomJin <sdpthf@naver.com>
2022-04-18 23:21:54 +0900

    🌧 :: formlogin disable & prevent preflight request

--- src/main/kotlin/com/example/kopring/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/kopring/global/security/SecurityConfig.kt
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.cors.CorsUtils
 
 @Configuration
 @EnableWebSecurity
@@ -22,8 +24,15 @@ class SecurityConfig(
         http
                 .cors().and()
                 .csrf().disable()
+                .formLogin().disable()
 
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
                 .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+
                 .antMatchers("/**").permitAll()
                 .anyRequest().denyAll()
 
