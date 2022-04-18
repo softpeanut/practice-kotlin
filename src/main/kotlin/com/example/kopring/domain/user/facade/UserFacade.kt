aaaa615a40d6c8fb5cdbd1c2dfa3de0de5b1db87
KimBeomJin <sdpthf@naver.com>
2022-04-18 23:07:05 +0900

    🌧 :: user facade

--- src/main/kotlin/com/example/kopring/domain/user/facade/UserFacade.kt
+++ src/main/kotlin/com/example/kopring/domain/user/facade/UserFacade.kt
@@ -4,6 +4,7 @@ import com.example.kopring.domain.user.entity.User
 import com.example.kopring.domain.user.exception.UserNotFoundException
 import com.example.kopring.domain.user.repository.UserRepository
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,7 +13,20 @@ class UserFacade(
 ) {
 
     fun getById(id: Long): User {
-        return userRepository.findByIdOrNull(id) ?: throw UserNotFoundException.EXCEPTION;
+        return userRepository.findByIdOrNull(id) ?: throw UserNotFoundException.EXCEPTION
+    }
+
+    fun getByAccountId(accountId: String): User {
+        return userRepository.findByAccountId(accountId) ?: throw UserNotFoundException.EXCEPTION
+    }
+
+    fun getCurrentUser(): User {
+        val currentUserAccountId = getAuthenticationName()
+        return getByAccountId(currentUserAccountId)
+    }
+
+    private fun getAuthenticationName(): String {
+        return SecurityContextHolder.getContext().authentication.name
     }
 
 }
