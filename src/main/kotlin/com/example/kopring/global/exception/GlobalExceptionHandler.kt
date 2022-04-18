d15b82498ddc63590e7003dbef0cd8b8d9973f26
KimBeomJin <sdpthf@naver.com>
2022-04-18 15:30:51 +0900

    🌱 :: GlobalExceptionHandler

---
+++ src/main/kotlin/com/example/kopring/global/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,20 @@
+package com.example.kopring.global.exception
+
+import com.example.kopring.global.exception.error.BaseErrorResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException::class)
+    fun customException(e: CustomException): ResponseEntity<BaseErrorResponse> {
+        return ResponseEntity(
+                BaseErrorResponse.of(e),
+                HttpStatus.valueOf(e.status)
+        )
+    }
+
+}
