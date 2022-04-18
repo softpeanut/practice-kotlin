2f7b1891c8edcf13c90484ad38628d23186a602f
KimBeomJin <sdpthf@naver.com>
2022-04-18 23:05:45 +0900

    🌱 :: AuthDetails

---
+++ src/main/kotlin/com/example/kopring/global/security/auth/AuthDetails.kt
@@ -0,0 +1,24 @@
+package com.example.kopring.global.security.auth
+
+import com.example.kopring.domain.user.entity.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+        private val user: User
+) : UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
+
+    override fun getPassword(): String = user.password
+
+    override fun getUsername(): String = user.accountId
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
