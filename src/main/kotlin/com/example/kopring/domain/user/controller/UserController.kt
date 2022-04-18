d78b736986c2d4d07f55dea92e48a9fa9d5a8c7d
KimBeomJin <sdpthf@naver.com>
2022-04-18 23:02:20 +0900

    🌧 :: user controller

--- src/main/kotlin/com/example/kopring/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/example/kopring/domain/user/controller/UserController.kt
@@ -1,8 +1,10 @@
 package com.example.kopring.domain.user.controller
 
+import com.example.kopring.domain.user.payload.request.LoginRequest
 import com.example.kopring.domain.user.payload.request.SaveUserRequest
 import com.example.kopring.domain.user.payload.response.UserInfoResponse
 import com.example.kopring.domain.user.service.UserService
+import com.example.kopring.global.security.jwt.payload.response.TokenResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -18,9 +20,20 @@ class UserController(
         userService.save(request)
     }
 
-    @GetMapping("/{user-id}")
+    @GetMapping("/info/{user-id}")
     fun getUserInfo(@PathVariable("user-id") userId: Long): UserInfoResponse {
         return userService.getUserInfo(userId)
+    }
+
+    @GetMapping("/info")
+    fun getMyInfo(): UserInfoResponse {
+        return userService.getMyInfo()
+    }
+
+    @PostMapping("/token")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun login(@RequestBody request: LoginRequest): TokenResponse {
+        return userService.login(request)
     }
 
 }
