dfef61bb3748b9c53473a063e20177cd9b3ae7ca
KimBeomJin <sdpthf@naver.com>
2022-04-18 23:08:04 +0900

    🌧 :: findByAccountId method

--- src/main/kotlin/com/example/kopring/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/example/kopring/domain/user/repository/UserRepository.kt
@@ -4,4 +4,5 @@ import com.example.kopring.domain.user.entity.User
 import org.springframework.data.repository.CrudRepository
 
 interface UserRepository : CrudRepository<User, Long> {
+    fun findByAccountId(accountId: String): User?
 }
